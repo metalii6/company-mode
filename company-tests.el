metalii6/company-mode

commit f517fc9faa78ca2d8c598745d4c63ba675e5e831

company--posn-col-row: use `posn-col-row'
Fixes #175

company-tests.el
@@ -586,6 +586,15 @@
       (put-text-property (point-min) (point-max) 'line-prefix "  ")
       (should (= (company--column) 2)))))
 
+(ert-deftest company-column-with-tabs ()
+  :tags '(interactive)
+  (with-temp-buffer
+    (save-window-excursion
+      (set-window-buffer nil (current-buffer))
+      (insert "|\t|\t|\t(")
+      (let ((tab-width 8))
+        (should (= (company--column) 25))))))
+
 (ert-deftest company-plainify ()
   (let ((tab-width 8))
     (should (equal-including-properties

company.el
@@ -785,7 +785,7 @@ means that `company-mode' is always turned on except in `message-mode' buffers."
   (push 31415926 unread-command-events))
 
 (defun company--posn-col-row (pos)
-  (let* ((col-row (posn-actual-col-row pos))
+  (let* ((col-row (posn-col-row pos))
          (col (car col-row))
          (row (cdr col-row)))
     (when header-line-format

commit edd4e2b622f5644cb5bd542746a23ccae93cf32e

Add test for the previous commit
Was broken in f517fc9.

company-tests.el
@@ -595,6 +595,15 @@
       (let ((tab-width 8))
         (should (= (company--column) 25))))))
 
+(ert-deftest company-row-with-header-line-format ()
+  :tags '(interactive)
+  (with-temp-buffer
+    (save-window-excursion
+      (set-window-buffer nil (current-buffer))
+      (should (= (company--row) 0))
+      (setq header-line-format "aaaaaaa")
+      (should (= (company--row) 0)))))
+
 (ert-deftest company-plainify ()
   (let ((tab-width 8))
     (should (equal-including-properties